522b01a989288348a812cd3c9a7f4102560f4cb0
vgrigorchik <vgrigorchik@gmail.ru>
2022-08-23 22:05:21 +0300

    создание проекта "список продуктов" на котлин андроид студио. 1. сделать дравер лайаут с регистрацией пользователей и редактированием списков покупок.

---
+++ app/src/main/java/com/vgrigorchik/spisok_pokupok/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.vgrigorchik.spisok_pokupok
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
